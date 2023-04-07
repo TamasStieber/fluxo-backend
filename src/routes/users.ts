8d18ed8198116d0c4c1c455b74bb6ccc0d183582
Tamas Stieber <stiebertamas@gmail.com>
2023-04-08 01:44:13 +0200

    Add Avatar and Password change from profile settings

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,8 +1,28 @@
 import { Router } from 'express';
 import User from '../models/user';
 import Post from '../models/post';
+import multer from 'multer';
+import bcrypt from 'bcrypt';
+import fs from 'fs';
+import * as Interfaces from '../interfaces/interfaces';
 
 const router = Router();
+
+const usersDir = './users';
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './temp');
+  },
+  filename: function (req, file, cb) {
+    const extension = '.' + file.originalname.split('.').pop();
+    const uniqueSuffix =
+      Date.now() + '-' + Math.round(Math.random() * 1e9) + extension;
+    cb(null, 'fluxo' + '-' + uniqueSuffix);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 router.get('/', async (req, res, next) => {
   try {
@@ -15,7 +35,8 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:id', async (req, res, next) => {
   try {
-    // const user = await User.findById(req.params.id);
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ error: 'User not found' });
     // res.status(200).json({ user });
     await User.findById(req.params.id)
       .populate('posts')
@@ -47,6 +68,51 @@ router.get('/:id/posts', async (req, res, next) => {
       });
   } catch {
     res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
+router.put('/:id', upload.single('profilePicture'), async (req, res, next) => {
+  const updatedUser = JSON.parse(req.body.data) as Interfaces.User;
+  const previousUser = await User.findById(req.params.id);
+
+  const fileName = req.file?.filename;
+  if (fileName) updatedUser.profilePictureUrl = fileName;
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, updatedUser);
+    const userDir = usersDir + `/${req.params.id}`;
+    if (fileName) {
+      fs.unlinkSync(userDir + '/photos/' + previousUser?.profilePictureUrl);
+      fs.renameSync('./temp/' + fileName, userDir + '/photos/' + fileName);
+    }
+    res.status(200).json(user);
+  } catch {
+    next(res.status(500).json({ error: 'Internal server error' }));
+  }
+});
+
+router.put('/:id/password', async (req, res, next) => {
+  const passwords = req.body;
+  const previousUser = await User.findById(req.params.id);
+
+  if (!previousUser) return res.status(404).json({ error: 'User not found' });
+
+  const isMatch = await bcrypt.compare(
+    passwords.oldPassword,
+    previousUser.password
+  );
+
+  if (!isMatch) return res.status(401).json({ error: 'Incorrect password!' });
+
+  const newPassword = await bcrypt.hash(passwords.newPassword, 10);
+
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, {
+      password: newPassword,
+    });
+    res.status(200).json(user);
+  } catch {
+    next(res.status(500).json({ error: 'Internal server error' }));
   }
 });
 
